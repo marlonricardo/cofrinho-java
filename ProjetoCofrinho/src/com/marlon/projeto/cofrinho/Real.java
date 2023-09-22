ae2edd27af51d569eb3a2df33af037ff68439b8c
marlonricardo <67763666+marlonricardo@users.noreply.github.com>
2023-09-22 18:01:01 -0300

    Update Real.java

--- ProjetoCofrinho/src/com/marlon/projeto/cofrinho/Real.java
+++ ProjetoCofrinho/src/com/marlon/projeto/cofrinho/Real.java
@@ -1,5 +1,5 @@
 package com.marlon.projeto.cofrinho;
-//classe Real que estende a classe abstrata Moedas, 
+//classe Real que extends a classe abstrata Moedas, 
 //que indica que a classe Real herda os atributos e métodos da classe Moedas.
 class Real extends Moedas {
     public Real(double valor) {
